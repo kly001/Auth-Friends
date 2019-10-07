019519e0f705d3cf94e4c4b8cf4777f60c8f8f19
kly001 <karenly1961@gmail.com>
2019-10-07 15:41:34 -0400

    Login.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from "./components/Login"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>Auth-Friends Project</h1>
+    <Login />
     </div>
   );
 }
--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const axiosWithAuth = () => {
+export const axiosWithAuth = () => {
     const token = localStorage.getItem("token")
 
     return axios.create({
-        baseURL:"http://localhost:5000",
+        baseURL:"http://localhost:5000/api",
         headers: {
             Authorization:token
         }
